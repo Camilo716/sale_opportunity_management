3d5c3e17e096b05c037efa1792a8f6ec2e8bebd7
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-28 00:30:32 +0000

    Se crea modelo inicial de prospecto

---
+++ prospect.py
@@ -0,0 +1,9 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.model import ModelSQL, ModelView, fields 
+
+class Prospect(ModelSQL, ModelView):
+    'Prospecto'
+    __name__ = 'call.prospect'
+
+    name = fields.Char('Name')
