c32b4ce31c95452aa3bc717ebc54dbdb0ad42acf
sinergia <alejandro.ayala@onecluster.org>
2023-09-04 17:39:20 -0500

    Fix: Se añade dominio en seguiemiento de prospectos por rol

---
+++ user.py
@@ -0,0 +1,17 @@
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+
+class User(metaclass=PoolMeta):
+    "User"
+    __name__ = 'res.user'
+
+    user_admin = fields.Boolean('Is Admin')
+
+    @classmethod
+    def __setup__(cls):
+        super(User, cls).__setup__()
+        cls._context_fields.insert(0, 'user_admin')
